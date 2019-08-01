d75411af4edef13f99b97cab809b8c041c7b7d93
Nathan Costea <cnlnat@yahoo.com>
2019-08-01 17:17:33 -0400

    Added a Web Api class.

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
