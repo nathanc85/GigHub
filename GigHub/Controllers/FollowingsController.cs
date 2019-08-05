8ad0bc4c89bcfad92fe38f8862597b6be024ebd1
Nathan Costea <cnlnat@yahoo.com>
2019-08-05 14:21:25 -0400

    Adding a Followings api controller.

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,44 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        ApplicationDbContext _context;
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var currentUser = User.Identity.GetUserId();
+            var exists = _context.Followings.Any(f => f.FolloweeId == dto.FolloweeId && f.FollowerId == currentUser);
+
+            if (exists)
+            {
+                return BadRequest("You already follow this artist.");
+            }
+
+            Following newFollowing = new Following()
+            {
+                FollowerId = currentUser,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(newFollowing);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
