cf40c38ad90eb3173275d53a11f8a8d13cfdfae0
Nathan Costea <cnlnat@yahoo.com>
2019-08-22 09:11:53 -0400

    New Notification api controller.

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Notification> GetNewNotifications()
+        {
+            var currentUser = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == currentUser)
+                .Select(un => un.Notification)
+                .Include(g => g.Gig.Artist)
+                .ToList();
+
+            return notifications;
+        }
+    }
+}
