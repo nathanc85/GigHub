4523a8299efeea157f5970528b0c0173a2179c44
NathanC <cnlnat@yahoo.com>
2019-10-30 14:23:59 -0400

    First Repository Test.

--- GigHub.Test/Persistence/Repositories/GigRepositoryTests.cs
+++ GigHub.Test/Persistence/Repositories/GigRepositoryTests.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using FluentAssertions;
+using GigHub.Core.Models;
 using GigHub.Persistence;
 using GigHub.Persistence.Repositories;
+using GigHub.Test.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -9,12 +14,29 @@ namespace GigHub.Test.Persistence.Repositories
     [TestClass]
     public class GigRepositoryTests
     {
-        private GigRepository _repository; 
+        private GigRepository _repository;
+        private Mock<DbSet<Gig>> _mockGigs;
         [TestInitialize]
         public void TestInitialize()
         {
             var mockContext = new Mock<IApplicationDbContext>();
+            _mockGigs = new Mock<DbSet<Gig>>();
+            mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
             _repository = new GigRepository(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void GetFutureGigsWithGenre_GigIsInThePast_ShouldNotBeReturned()
+        {
+            // Arrange
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(-1), ArtistId = "1" };
+            _mockGigs.SetSource(new List<Gig> { gig });
+
+            // Act
+            var gigs = _repository.GetFutureGigsWithGenre("1");
+
+            // Assert
+            gigs.Should().BeEmpty();
         }
     }
 }
