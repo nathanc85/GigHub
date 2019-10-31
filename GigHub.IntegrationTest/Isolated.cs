8dadf218fe450675f0f045fbdc251bb05ae1891f
NathanC <cnlnat@yahoo.com>
2019-10-31 10:08:16 -0400

    Mocking the curent user in integration tests and creating an isolated test for scoped transactions that will keep the test data clean.

---
+++ GigHub.IntegrationTest/Isolated.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace GigHub.IntegrationTest
+{
+    public class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+        public ActionTargets Targets
+        { 
+            get { return ActionTargets.Test; }
+        }
+
+        public void AfterTest(TestDetails testDetails)
+        {
+            _transactionScope.Dispose();
+        }
+
+        public void BeforeTest(TestDetails testDetails)
+        {
+            _transactionScope = new TransactionScope();
+        }
+    }
+}
