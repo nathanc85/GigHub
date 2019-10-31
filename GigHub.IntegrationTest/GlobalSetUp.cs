3aa4331c00e39e2a2ad61a73451039cdb906a5cc
NathanC <cnlnat@yahoo.com>
2019-10-31 09:40:19 -0400

    Setting up the integration test database and seeding.

--- GigHub.IntegrationTest/GlobalSetUp.cs
+++ GigHub.IntegrationTest/GlobalSetUp.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using GigHub.Core.Models;
+using GigHub.Persistence;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
@@ -14,10 +16,29 @@ namespace GigHub.IntegrationTest
         [SetUp]
         public void SetUp()
         {
+            MigrateDbToLatest();
+
+            Seed();
+        }
+
+        private static void MigrateDbToLatest()
+        {
             // If we don't have a database, it will be created and migrated to the latest version.
             var configuration = new GigHub.Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "user1@user1.com", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "user2@user2.com", PasswordHash = "-" });
+            context.SaveChanges();
         }
     }
 }
