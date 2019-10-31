ae3d11b08d9ef98ab3bbfc714ec05e91d6974b9b
NathanC <cnlnat@yahoo.com>
2019-10-31 14:22:53 -0400

    Database update Integration test.

--- GigHub.IntegrationTest/Controllers/GigsControllerTests.cs
+++ GigHub.IntegrationTest/Controllers/GigsControllerTests.cs
@@ -11,6 +11,7 @@ using GigHub.Core.Models;
 using System.Web.Mvc;
 using System.Collections;
 using FluentAssertions;
+using GigHub.Core.ViewModels;
 
 namespace GigHub.IntegrationTest.Controllers
 {
@@ -52,6 +53,38 @@ namespace GigHub.IntegrationTest.Controllers
 
             // Assert
             (result.ViewData.Model as IEnumerable<Gig>)?.Should().HaveCount(1);
+        }
+
+        [Test, Isolated]
+        public void Update_WhenCalled_ShouldUpdateTheGig()
+        {
+            // Arrange
+            // Mock the current user --> use the extension we created.
+            var user = _context.Users.First();
+            _controller.MockCurrentUser(user.Id, user.UserName);
+
+            // Create a new Gig.
+            var genre = _context.Genres.Single( g => g.Id == 1);
+            var gig = new Gig { Artist = user, DateTime = DateTime.Now.AddDays(2), Genre = genre, Venue = "-" };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            // Act
+            var result = _controller.Update(new GigFormViewModel
+                {
+                    Id = gig.Id,
+                    Venue = "San Jose Teather",
+                    Date = DateTime.Today.AddMonths(3).ToString("MM/dd/yyyy"),
+                    Time = "20:00",
+                    Genre = 2
+
+            });
+
+            // Assert
+            _context.Entry(gig).Reload();
+            gig.DateTime.Should().Be(DateTime.Today.AddMonths(3).AddHours(20));
+            gig.Venue.Should().Be("San Jose Teather");
+            gig.GenreId.Should().Be(2);
         }
     }
 }
